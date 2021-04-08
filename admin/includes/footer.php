ed06be45f52169ef73e35e599c9d889d08b4ac58
Sushant Mohite <69734778+Darkseidsam7@users.noreply.github.com>
2021-04-08 14:39:47 +0530

    libsystem v1

---
+++ admin/includes/footer.php
@@ -0,0 +1,6 @@
+<footer class="main-footer">
+    <div class="pull-right hidden-xs">
+     
+    </div>
+    <strong>&copy; 2019 - Library Management System | Brought To You By <a href="https://github.com/Darkseidsam7/">github</a></strong>
+</footer>
---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Open Admin Panel and Add/View user to know Login ID</b>
+      </div>
+      <strong>&copy; 2019 - Library Management System | Brought To You By <a href="https://github.com/Darkseidsam7/">github</a></strong>
+      </div>
+    <!-- /.container -->
+</footer>
